a7c466867105b2f0cdc2136a7a66d8cb2e345b9d
Sol-Project <63589740+sol-pro@users.noreply.github.com>
2024-11-05 09:31:46 +0900

    main.py を更新

---
+++ main.py
@@ -0,0 +1,11 @@
+Increases the number of simultaneous connections on Twitch.
+
+It is written in python and runs on threads, and can increase the number of users by 80-120 with about 10 threads.
+
+If you want the source code, please contact us.
+
+discord | solton_e
+
+Discord invite
+
+There are no invites yet.
